e7d110e8c89f361d18daac1105b7ecff90da7210
nada-pont-com <42653635+nada-pont-com@users.noreply.github.com>
2022-01-02 22:17:01 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, Intents, Message } = require("discord.js");
+const WOKCommands = require("wokcommands");
+const path = require("path");
+const config = require("./config.json");
+require("./data_base.js").start();
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ]
+});
+
+client.on('ready', async (cliente) => {
+    new WOKCommands(client, {
+        commandDir: path.join(__dirname, 'comandos'),
+        testServers: ['923927113424318485'],
+        // typeScript: true,
+    });
+
+});
+
+client.login(config.BOT_TOKEN);
